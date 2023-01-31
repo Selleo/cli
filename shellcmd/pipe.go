48b067e655cd42ae088ea94b4196b42aa2508cd8
qbart <wojtowicz.bartlomiej@gmail.com>
2023-01-31 01:06:09 +0100

    Experiment with command pipe

--- shellcmd/pipe.go
+++ shellcmd/pipe.go
@@ -1,9 +1,12 @@
 package shellcmd
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io"
+	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/wzshiming/ctc"
@@ -17,5 +20,86 @@ func Pipe(ctx context.Context, w io.Writer, secrets map[string]string, args []st
 		strings.Join(args, " "),
 		ctc.Reset,
 	)
-	return nil
+	cmdName := args[0]
+	cmdArgs := args[1:]
+	cmd := exec.CommandContext(ctx, cmdName, cmdArgs...)
+	envs := []string{}
+	envs = append(envs, os.Environ()...)
+	for k, v := range secrets {
+		// TODO: needs escaping
+		envs = append(envs, fmt.Sprint(k, "=", v))
+	}
+	cmd.Env = envs
+	cmd.Stdin = os.Stdin
+
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	errOut, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	cancelCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go func() {
+		defer out.Close()
+
+		reader := bufio.NewReader(out)
+
+	LOOP:
+		for {
+			select {
+
+			case <-cancelCtx.Done():
+				fmt.Println("stopping stdout pipe")
+				break LOOP
+
+			default:
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					cancel()
+					return
+				}
+				fmt.Fprintf(w, line)
+			}
+		}
+	}()
+	go func() {
+		defer errOut.Close()
+
+		reader := bufio.NewReader(errOut)
+
+	LOOP:
+		for {
+			select {
+
+			case <-cancelCtx.Done():
+				fmt.Fprintf(w, "[stderr] stopping stderr pipe\n")
+				break LOOP
+
+			default:
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					fmt.Fprintf(w, "[stderr] err line reading\n")
+					cancel()
+					return
+				}
+				fmt.Fprintf(w, "%s%s%s", ctc.ForegroundRed, line, ctc.Reset)
+			}
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	err = cmd.Wait()
+	// TODO: add signal notify
+
+	fmt.Fprintf(w, "Stopping\n")
+
+	return err
 }
