5e6ad7b2b2191c296e089e0a53f5e8694968f554
qbart <wojtowicz.bartlomiej@gmail.com>
2023-02-05 17:47:27 +0100

    Add templates

---
+++ templates/terraform/iam.tf
@@ -0,0 +1,15 @@
+module "iam_app" {
+  source  = "Selleo/iam/aws//modules/user-with-access-key"
+  version = "0.6.0"
+
+  name = "{{ .IAMApp }}"
+}
+
+module "iam_app_allow_s3_read_write" {
+  source  = "Selleo/iam/aws//modules/s3-read-write"
+  version = "0.5.0"
+
+  bucket_arn  = aws_s3_bucket.storage.arn
+  name_prefix = "{{ .Namespace }}-{{ .Stage }}-{{ .Name }}"
+  users       = [module.iam_app.name]
+}
---
+++ templates/terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "{{ .Region }}"
+}
