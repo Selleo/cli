bfa67c38f25084f82542cc7a23fd6f85add4a449
qbart <wojtowicz.bartlomiej@gmail.com>
2024-09-09 18:49:40 +0200

    feat: Download TF modules

--- go.mod
+++ go.mod
@@ -15,7 +15,11 @@ require (
 )
 
 require (
+	github.com/briandowns/spinner v1.23.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/google/go-github/v64 v64.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -28,5 +32,6 @@ require (
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/Selleo/cli/random"
 	"github.com/Selleo/cli/selleo"
 	"github.com/Selleo/cli/shellcmd"
+	"github.com/Selleo/cli/templates"
 	"github.com/urfave/cli/v2"
 	"github.com/wzshiming/ctc"
 )
@@ -179,136 +180,24 @@ func main() {
 						Usage: "Terraform related generators",
 						Subcommands: []*cli.Command{
 							{
-								Name:  "app",
-								Usage: "Generate single app envrionemnt",
+								Name:  "modules",
+								Usage: "Generate latest Terraform modules",
 								Flags: []cli.Flag{
-									&cli.StringFlag{Name: "tf-cloud-org", Usage: "Terraform Cloud organization", Required: true},
-									&cli.StringFlag{Name: "tf-cloud-workspace", Usage: "Terraform Cloud workspace", Required: true},
-									&cli.StringFlag{Name: "region", Usage: "AWS Region for resources", Required: true},
-									&cli.StringFlag{Name: "stage", Usage: "App stage", Required: true},
-									&cli.StringFlag{Name: "namespace", Usage: "App namespace", Required: true},
-									&cli.StringFlag{Name: "name", Usage: "App ID", Required: true},
-									&cli.StringFlag{Name: "domain", Usage: "Domain", Required: true},
-									&cli.StringFlag{Name: "subdomain", Usage: "Subdomain", Required: true},
+									&cli.BoolFlag{Name: "force", Usage: "Overwrite existing files", Required: false, Value: false, Aliases: []string{"f"}},
+									&cli.StringFlag{Name: "dir", Usage: "Output directory", Required: false, Aliases: []string{"d"}, Value: "terraform"},
 								},
 								Action: func(c *cli.Context) error {
-									gen := generators.Terraform{
-										TerraformCloudOrganization: c.String("tf-cloud-org"),
-										TerraformCloudWorkspace:    c.String("tf-cloud-workspace"),
-										Region:                     c.String("region"),
-										Namespace:                  c.String("namespace"),
-										Stage:                      c.String("stage"),
-										Name:                       c.String("name"),
-										Subdomain:                  c.String("subdomain"),
-										Domain:                     c.String("domain"),
-										IAMCI:                      fmt.Sprintf("ci-%s-%s", c.String("namespace"), c.String("stage")),
-										IAMApp:                     fmt.Sprintf("%s-%s-%s", c.String("namespace"), c.String("stage"), c.String("name")),
-										ECSInstanceType:            "t3.medium",
-										ECSMinSize:                 1,
-										ECSServiceMinMemory:        256,
-										ECSServiceMaxMemory:        1024,
-										ECSServiceCpu:              1024,
-										ECSServicePort:             3000,
-										ECSOneOffs:                 []string{},
-										LBName:                     fmt.Sprintf("%s-%s-%s", c.String("namespace"), c.String("stage"), c.String("name")),
-										DBName:                     c.String("name"),
-										DBIdentifier:               c.String("name"),
-										DBUser:                     "app",
-										DBMultiAZ:                  false,
-										DBApplyImmediately:         false,
-										BucketName:                 fmt.Sprintf("%s-%s-%s-storage", c.String("namespace"), c.String("stage"), c.String("name")),
-									}
-									return gen.Render(generators.New(embededTemplates))
-								},
-							},
-						},
-					},
-					{
-						Name:  "docker",
-						Usage: "Docker related generators",
-						Subcommands: []*cli.Command{
-							{
-								Name:  "rails",
-								Usage: "Generate Dockerfile with entrypoint",
-								Action: func(c *cli.Context) error {
-									gen := generators.Docker{
-										CmdServer: "rails server",
-										OneOffs: map[string]string{
-											"migrate": "rails db:migrate",
-										},
-									}
-									return gen.Render(generators.New(embededTemplates))
-								},
-							},
-						},
-					},
-					{
-						Name:  "github",
-						Usage: "GitHub workflows",
-						Subcommands: []*cli.Command{
-							{
-								Name:  "backend",
-								Usage: "Generate GitHub actions for backend",
-								Flags: []cli.Flag{
-									&cli.StringFlag{Name: "workdir", Usage: "Working directory (source root)", Required: true},
-									&cli.StringFlag{Name: "domain", Usage: "Application domain", Required: true},
-									&cli.StringFlag{Name: "subdomain", Usage: "Application subdomain", Required: true},
-									&cli.StringFlag{Name: "region", Usage: "AWS Region for S3", Required: true},
-									&cli.StringFlag{Name: "ecs-cluster", Usage: "ECS cluster name", Required: true},
-									&cli.StringFlag{Name: "ecs-service", Usage: "ECS service name", Required: true},
-									&cli.StringFlag{Name: "stage", Usage: "Application environment", Required: true},
-									/// optional
-									&cli.BoolFlag{Name: "tag-release", Usage: "Trigger CI on tag release", Required: false, Value: false},
-									&cli.StringSliceFlag{Name: "one-off", Usage: "One-off commands (multiple use of flag allowed)", Required: false},
-								},
-								Action: func(c *cli.Context) error {
-									tpls := generators.New(embededTemplates)
-									gen := generators.GitHub{
-										CITagTrigger: c.Bool("tag-release"),
-										CIBranch:     "main",
-										CIWorkingDir: c.String("workdir"),
-										Stage:        c.String("stage"),
-										Domain:       c.String("domain"),
-										Subdomain:    c.String("subdomain"),
-										Region:       c.String("region"),
-										ECSCluster:   c.String("ecs-cluster"),
-										ECSService:   c.String("ecs-service"),
-										ECSOneOffs:   c.StringSlice("one-off"),
-									}
-									if err := gen.RenderBackend(tpls); err != nil {
+									downloader, err := templates.NewDownloader()
+									if err != nil {
 										return err
 									}
-									return nil
-								},
-							},
-							{
-								Name:  "frontend",
-								Usage: "Generate GitHub actions for CDN",
-								Flags: []cli.Flag{
-									&cli.StringFlag{Name: "workdir", Usage: "Working directory (source root)", Required: true},
-									&cli.StringFlag{Name: "domain", Usage: "Application domain", Required: true},
-									&cli.StringFlag{Name: "region", Usage: "AWS Region for S3", Required: true},
-									&cli.StringFlag{Name: "app_id", Usage: "App ID specified in Terraform", Required: true},
-									&cli.StringFlag{Name: "stage", Usage: "Application environment", Required: true},
-									/// optional
-									&cli.BoolFlag{Name: "tag-release", Usage: "Trigger CI on tag release", Required: false, Value: false},
-								},
-								Action: func(c *cli.Context) error {
-									tpls := generators.New(embededTemplates)
-									gen := generators.GitHub{
-										CITagTrigger: c.Bool("tag-release"),
-										CIBranch:     "main",
-										CIWorkingDir: c.String("workdir"),
-										Stage:        c.String("stage"),
-										Domain:       c.String("domain"),
-										Region:       c.String("region"),
-										AppID:        c.String("app_id"),
-									}
-									if err := gen.RenderFrontend(tpls); err != nil {
-										return err
-									}
-
-									return nil
+									return downloader.Download(c.Context, &templates.DownloadInput{
+										RepoOwner:   "Selleo",
+										RepoName:    "terraform-aws-modules",
+										FolderPath:  "modules",
+										Destination: c.String("dir"),
+										Force:       c.Bool("force"),
+									})
 								},
 							},
 						},
